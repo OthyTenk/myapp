ddeb895d463d6b32bf42ad6f874f137c5c3f6b17
OthyTenk <othy.tenk@gmail.com>
2017-12-26 23:13:37 +0800

    Цэс болон статус бар нэмсэн

--- index.php
+++ index.php
@@ -10,6 +10,31 @@ session_start();
   <title>test home</title>
 </head>
 <body>
+  <nav class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.php">Testing Home Page</a>
+          </div>
+          <div class="collapse navbar-collapse" id="navbar1">
+              <ul class="nav navbar-nav navbar-right">
+                  <?php if (isset($_SESSION['usr_id'])) { ?>
+                  <li><p class="navbar-text">Хэрэглэгч <?php echo $_SESSION['usr_name']; ?></p></li>
+                  <li><a href="logout.php">Гарах</a></li>
+                  <?php } else { ?>
+                  <li><a href="auth/index.php">Нэвтрэх</a></li>
+                  <li><a href="auth/reg.php">Бүртгүүлэх</a></li>
+                  <?php } ?>
+              </ul>
+          </div>
+      </div>
+  </nav>
+
 
 </body>
 </html>
