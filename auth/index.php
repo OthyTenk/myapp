34bf694b7b8046527c8cbe17f2224183a7d96fc8
btc <othy.tenk@gmail.com>
2017-12-26 17:36:52 +0800

    шинээр нэмсэн

---
+++ auth/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Нэвтрэх</title>
+</head>
+<body>
+
+<form method="post" action="auth/login.php" id="" name="">
+<table>
+  <tr>
+    <td colspan="2">Нэвтрэх</td>
+  </tr>
+  <tr>
+    <td>Имэйл:</td>
+    <td>
+      <input type="email" name="email" id="email" size="30" maxlength="40"/>
+    </td>
+  </tr>
+  <tr>
+    <td>Нууц үг:</td>
+    <td>
+      <input type="password" name="pwd" id="pwd" size="30" maxlength="60"/>
+    </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>
+      <input type="submit" name="login" id="login" value="Нэвтрэх"/>
+    </td>
+  </tr>
+</table>
+<form>
+
+</body>
+</html>
